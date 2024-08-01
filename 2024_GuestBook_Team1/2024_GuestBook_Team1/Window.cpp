820a4d9c005b0e9af6437e2ac992309aee684bfe
semicolon213 <khk213624@gmail.com>
2024-08-01 23:14:54 +0900

    코드 분할 및 최적화

--- 2024_GuestBook_Team1/2024_GuestBook_Team1/Window.cpp
+++ 2024_GuestBook_Team1/2024_GuestBook_Team1/Window.cpp
@@ -1,1 +1,208 @@
 #include "Window.h"
+
+// 멤버 변수 초기화
+unique_ptr<Window> Window::sinTonIns = nullptr;
+once_flag Window::flag;
+
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM Window::MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = StaticWndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY2024GUESTBOOKTEAM1));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY2024GUESTBOOKTEAM1);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL Window::InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+LRESULT Window::StaticWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    return sinTonIns->WndProc(hWnd, message, wParam, lParam);
+}
+
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+/// @brief WndProc 전역변수 선언
+bool LBState = false;
+int px, py;
+
+LRESULT Window::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    hdc = GetDC(hWnd);
+    int x, y;
+
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // 메뉴 선택을 구문 분석합니다:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+
+    case WM_LBUTTONDOWN:
+        LBState = true;
+        px = LOWORD(lParam);
+        py = HIWORD(lParam);
+
+        record(lParam, (DWORD)GetTickCount64(), message);
+
+        break;
+
+    case WM_MOUSEMOVE:
+        if (!LBState)
+            break;
+        x = LOWORD(lParam);
+        y = HIWORD(lParam);
+        MoveToEx(hdc, px, py, NULL);
+        LineTo(hdc, x, y);
+
+        px = x;
+        py = y;
+
+        record(lParam, (DWORD)GetTickCount64(), message);
+
+        break;
+
+    case WM_LBUTTONUP:
+        LBState = false;
+        record(lParam, (DWORD)GetTickCount64(), message);
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+        bool LBState = false;
+        int x, y;
+        for (const auto& record : drawLInfo.pInfo)
+        {
+            x = LOWORD(record.lParam);
+            y = HIWORD(record.lParam);
+
+            switch (record.state)
+            {
+            case WM_LBUTTONDOWN:
+                LBState = true;
+                MoveToEx(hdc, x, y, NULL);
+                LineTo(hdc, x, y);
+                break;
+
+            case WM_MOUSEMOVE:
+                LineTo(hdc, x, y);
+                break;
+            case WM_LBUTTONUP:
+                LBState = false;
+                break;
+            default:
+                break;
+            }
+        }
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    ReleaseDC(hWnd, hdc);
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR Window::About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+
+    }
+    return (INT_PTR)FALSE;
+}
+
+Window* Window::GetInstance()
+{
+    call_once(flag, []          ///익명함수
+        {
+            sinTonIns.reset(new Window);
+        });
+    return sinTonIns.get();
+}
--- 2024_GuestBook_Team1/2024_GuestBook_Team1/Window.h
+++ 2024_GuestBook_Team1/2024_GuestBook_Team1/Window.h
@@ -1,23 +1,47 @@
 #pragma once
 
+#include <memory>
+#include <mutex>
+
 #include "framework.h"
+#include "GB_Function.h"
+#include "Resource.h"
 
 #define MAX_LOADSTRING 100
+
+using namespace std;
+
+extern LINFO drawLInfo;
 
 class Window
 {
 private :
 	HINSTANCE hInst;
 	HWND hWnd;
-
+	
+	static LRESULT CALLBACK StaticWndProc(HWND, UINT, WPARAM, LPARAM);
 	LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 	static INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
+
+	static unique_ptr<Window> sinTonIns;
+	static once_flag flag;
+
 public :
-	Window() = default;					
+	Window() = default;
 	~Window() = default;
 
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
+
+	WCHAR szTitle[MAX_LOADSTRING];
+	WCHAR szWindowClass[MAX_LOADSTRING];
+
+	ATOM MyRegisterClass(HINSTANCE);
+	BOOL InitInstance(HINSTANCE, int);
+
+	static Window* GetInstance();
+
+
 };
 
