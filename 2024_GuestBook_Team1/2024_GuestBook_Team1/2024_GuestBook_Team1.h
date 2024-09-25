169871fdb18e2ed26dba680b91d10e1f777f7f00
semicolon213 <khk213624@gmail.com>
2024-09-25 11:33:06 +0900

    메인 싱글톤 삭제.. 김창섭의 정상화...

--- 2024_GuestBook_Team1/2024_GuestBook_Team1/2024_GuestBook_Team1.h
+++ 2024_GuestBook_Team1/2024_GuestBook_Team1/2024_GuestBook_Team1.h
@@ -1,4 +1,21 @@
 ﻿#pragma once
 
+#ifndef WINDOW_H
+#define WINDOW_H
+
 #include <Windows.h>
-#include "Window.h"
+#include <mutex>
+#include <memory>
+#include <thread>
+
+#include "BlueWindow.h"
+#include "framework.h"
+#include "Resource.h"
+#include "DrowWindow.h"
+#include "FileManager.h"
+#include "Function.h"
+#include "ColorPalette.h"
+#include "Struct.h"
+
+
+#endif
