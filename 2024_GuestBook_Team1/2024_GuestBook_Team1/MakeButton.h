5415a0b3d2813ac62c9ab9f0664eab231ab6bbea
Seungho1201 <ohseungho1201@gmail.com>
2024-10-23 20:07:07 +0900

    버튼 재구성 및 버튼 생성 클래스 추가

--- 2024_GuestBook_Team1/2024_GuestBook_Team1/MakeButton.h
+++ 2024_GuestBook_Team1/2024_GuestBook_Team1/MakeButton.h
@@ -1,25 +1,30 @@
 #pragma once
 #include "framework.h"
-
-
-
-
+#include <vector>
+#include "Resource.h"
 class MakeButton 
 {
-public:
-	RECT rectButton;
-
 private:
 	int icon;
 
 public:
+	RECT rectButton;
+	bool toggleState;
 
+	MakeButton();
 	MakeButton(int left, int top, int right, int bottom);
 
 	void drawRectButton(HDC tHdc, int icon);
 	void drawEllipseButton(HDC tHdc);
 
+	void setCoordinate(int left, int top, int right, int bottom);
+
+	void clickEffectPen(int icon,int clickIcon, HDC tHdc);
+
+	void clickEffectPen(int icon, HDC tHdc);
+	
+	void doubleImgButton(HDC tHdc, int icon1, int icon2);
+	
 protected:
 	void buttonLoadImage(int icon, HDC tHdc);
-
 };
